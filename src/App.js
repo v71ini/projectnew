3ad893420b5342393d25a4cbc06995b91c0665a7
v71ini <94135003+v71ini@users.noreply.github.com>
2023-08-18 11:46:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Posts List</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
